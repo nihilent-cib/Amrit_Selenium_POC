58dc7157ffa56e75df79f38422680256ab80f949
Amrit Rai <mail.amritrai@gmail.com>
2021-04-16 17:59:00 +0530

    Commited new project

---
+++ GIT_Training/src/test/java/com/git/testpackage/training.java
@@ -0,0 +1,16 @@
+package com.git.testpackage;
+
+public class training {
+
+	public static void main(String[] args) {
+		
+		
+		int arr []= {32,4,3,45,545,5145,54,5};
+		
+		int len= arr.length;
+		System.out.println(len);
+		
+
+	}
+
+}
