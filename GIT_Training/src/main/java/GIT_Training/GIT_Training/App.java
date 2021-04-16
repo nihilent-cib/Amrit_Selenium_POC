5c36bdfcfc73897ca6a3ff95f1933ac53d8b6cad
Amrit Rai <mail.amritrai@gmail.com>
2021-04-16 12:24:29 +0530

    Git Commit the project

---
+++ GIT_Training/src/main/java/GIT_Training/GIT_Training/App.java
@@ -0,0 +1,13 @@
+package GIT_Training.GIT_Training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
